20b5c848f91dd78303b43e5215ab987f7ee463a7
Gaurav Kamboj <gaurav.kamboj@localFlipkartGaurav.local>
2023-05-03 17:01:40 +0530

    lighthouse

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,7 +4,7 @@ module.exports = {
         target: 'temporary-public-storage',
       },
       collect:{
-        settings: {skipAudits: []}
+        staticDistDir: './build',
       },
       assert: {
         preset: 'lighthouse:no-pwa',
@@ -17,11 +17,11 @@ module.exports = {
           "tap-targets":"off",
           "unused-javascript":"off",
           "render-blocking-resources":"Off",
-          "uses-long-cache-ttl":"off"
-    //       'categories:performance': ['error', { minScore: 0.95 }],
-    //       'categories:accessibility': ['error', { minScore: 0.95 }],
-    //       'categories:best-practices': ['error', { minScore: 0.95 }],
-    //       'categories:seo': ['error', { minScore: 1 }],
+          "uses-long-cache-ttl":"off",
+          'categories:performance': ['error', { minScore: 0.95 }],
+          'categories:accessibility': ['error', { minScore: 0.95 }],
+          'categories:best-practices': ['error', { minScore: 0.95 }],
+          'categories:seo': ['error', { minScore: 1 }],
         },
       },
     },
